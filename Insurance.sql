b2d9f96aab8003f63075544e66c5757819f42e5b
Ganesh369Kumbhar <123569700+Ganesh369Kumbhar@users.noreply.github.com>
2023-08-04 16:16:15 +0530

    Insurance

---
+++ Insurance.sql
@@ -0,0 +1,10 @@
+create database Insurance;
+use Insurance;
+Select * from brokerage;
+Select * from fees;
+select * from individualbudgets;
+select * from individualbudgets;
+select * from invoice;
+select * from meeting;
+select * from opportunity;
+
